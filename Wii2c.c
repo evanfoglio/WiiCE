949d4270ac5a99849355170493101534a34a8e6a
osiris.thomas <osiris.thomas@maine.edu>
2021-11-29 16:58:01 -0500

    Added function to initialize talking to the Nunchuck

---
+++ Wii2c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+//#include <wiringPi.h>
+//#include <wiringPiI2C.h>
+#include <errno.h>
+
+void Wii2c_setup(void) 
+{
+    int NUNCHUCK_DEVICE = 0x52;
+    int fd, result;
+    unsigned char buffer[100] = {0};
+    
+    // Open i2c bus
+    fd = open("/dev/i2c-1", O_RDWR) 
+    if (fd < 0) {
+        printf("Error setting up I2C: %d\n", errno);
+        exit(-1);
+    }
+    
+    // Set device address
+    result = ioctl(fd, I2C_SLAVE, 0x70);
+    
+    //buffer[0x40] = 0x00;
+    result = write(fd, buffer, 100);    
+    if (result == 0) {
+        printf("Error writing to device: %d\n", errno);
+        exit(-2);
+    }
+
+    usleep(500);
+
+    }
+}
