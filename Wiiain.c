fa0fe8d17adbb9bc8b02297bc0e8cca147c44d7d
osiris.thomas <osiris.thomas@maine.edu>
2021-11-29 17:23:59 -0500

    Added contents to forever loop to read in the i2c values from the nunchuck, replace with normal interface later

--- Wiiain.c
+++ Wiiain.c
@@ -7,22 +7,32 @@
 
 #include "linux/gpio.h"
 #include "sys/ioctl.h"
+#include "Wii2c.h"
+#include "WiiPIO.h"
 
 int init(int gpiovals[14]);
 
 int main(int argc, char **argv) {
 
-	int gpiovals[14] = {7, 17, 27, 22, 10, 9, 11, // X-axis Values
-			   14, 15, 18, 23, 24, 25, 8};// Y-axis Values
-	//initWiiPIO(int gpiovals[14]){
+    int gpiovals[14] = {7, 17, 27, 22, 10, 9, 11, // X-axis Values			   
+                        14, 15, 18, 23, 24, 25, 8};// Y-axis Values
 
-	//Assuming everything is set up below here,
+    int bytes[6];
+    int i;
 
+    // Initialize the Nunchuck's I2C settings
+    Wii2c_setup();
 
-	while(true) {
+    while(1) {
+        
+        wiringPiI2CWrite(fd, 0x00);
+        delayMicroseconds(500);
+        for (i=0; i<6; i++) {
+            bytes[i] = wiringPiI2CRead(fd);
+        }
 
-		//Read in I2C values
-
+        int joyX = bytes[0];
+        int joyY = bytes[1];
 
 		//Write out to LEDs
 
