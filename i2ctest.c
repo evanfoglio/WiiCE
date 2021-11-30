3abdcafd1f3e50577cbe7db646506797eb13e73b
osiris.thomas <osiris.thomas@maine.edu>
2021-11-30 15:42:49 -0500

    fix

--- i2ctest.c
+++ i2ctest.c
@@ -10,36 +10,36 @@
 #include "WiiPIO.h"
 
 //int init(int gpiovals[14]);
-
+void lightUpAll(struct gpiohandle_request * req, int * gpios);
 int main(int argc, char **argv) {
 
     int rv;
     int result;
     int bytes_read;
-    int fd;
-    fd = open("/dev/gpiochip0",O_RDWR);
+    //int fd;
+    //fd = open("/dev/gpiochip0",O_RDWR);
 
 
     // First half of array is for X axis, second half is for Y axis
-    int gpiovals[14] = {4, 17, 27, 22, 10, 9, 11, 14, 15, 18, 23, 24, 25, 8};
-    struct gpiohandle_request * req = WiiPIOinit(gpiovals);
+    int gpiovals[14] = {17, 27, 22, 10, 9, 5, 6, 14, 15, 18, 23, 24, 25, 8};
+    
+    struct gpiohandle_request req[14];
+    WiiPIOinit(gpiovals, req);
+
+    //struct gpiohandle_data data;
+    //memset(&data,0,sizeof(struct gpiohandle_data)); 
+    //data.values[0]=1; // value to output (0 or 1)
+    //wirtes first gpio in array high, 4 doesnt work?
+    //rv=ioctl( req[0].fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
     
 
-    rv = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &(req[0]));
-    struct gpiohandle_data data;
-    data.values[0]=1; // value to output (0 or 1)
-    
-    rv=ioctl(req[0].fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
-    
     // Nunchuck returns 6 bytes
     unsigned char nc_data[6];
-    
+    unsigned char joyX, joyY; 
     int i;
-    unsigned char joyX, joyY;
     unsigned char buffer = 0x00;
     // Initialize the Nunchuck's I2C settings
-    fd = Wii2c_setup();
-    
+    int fd = Wii2c_setup();
     while(1) {
         
         result = write(fd, buffer, 1);
@@ -51,10 +51,62 @@ int main(int argc, char **argv) {
 
         joyX = nc_data[0];
         joyY = nc_data[1];
-        printf("X: %d\tY: %d\n", joyX, joyY);
+        printf("X: %3d\tY:%3d\n", joyX, joyY);
 
 	//Write out to LEDs
 
     }
     return 0;
 }
+
+/*
+ * This function takes in the array of GPIOs and 
+ * assuming they have all been initialized,
+ * turns all of the GPIOs on
+ */
+void lightUpAll(struct gpiohandle_request * req, int * gpios) {
+
+    int rv;
+    struct gpiohandle_data data;
+    memset(&data,0,sizeof(struct gpiohandle_data));
+    data.values[0]=1; // value to output (0 or 1)
+
+    for(int i = 0; i < 14; i++) {
+
+	rv = ioctl(req[i].fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data); 
+
+    }
+
+}
+
+
+
+void ledCtrlX(int value) {
+
+	
+
+}
+
+void ledCtrlY(int value) {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
