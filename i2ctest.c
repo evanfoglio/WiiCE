9c557c54a2d742f725b170e23b308dc0e036100f
osiris.thomas <osiris.thomas@maine.edu>
2021-11-30 17:17:03 -0500

    bepis

--- i2ctest.c
+++ i2ctest.c
@@ -9,81 +9,57 @@
 #include <sys/fcntl.h> 
 #include <sys/stat.h>
 #include <sys/ioctl.h>  
-#define I2C_SLAVE       0x0703
-#define I2C_BUS       0x0720  /* SMBus-level access */
-
-#define I2C_BUS_READ  1
-#define I2C_BUS_WRITE 0
-
-// SMBus transaction types
-
-#define I2C_BUS_QUICK             0
-#define I2C_BUS_BYTE              1
-#define I2C_BUS_BYTE_DATA         2 
-#define I2C_BUS_WORD_DATA         3
-#define I2C_BUS_PROC_CALL         4
-#define I2C_BUS_BLOCK_DATA        5
-#define I2C_BUS_I2C_BLOCK_BROKEN  6
-#define I2C_BUS_BLOCK_PROC_CALL   7           /* SMBus 2.0 */
-#define I2C_BUS_I2C_BLOCK_DATA    8
-#define I2C_BUS_BLOCK_MAX     32      /* As specified in SMBus standard */    
-#define I2C_BUS_I2C_BLOCK_MAX 32      /* Not specified but we use same structure */
-
-union i2c_data
-{
-  uint8_t  byte;
-  uint16_t word;
-  uint8_t  block[I2C_BUS_BLOCK_MAX + 2];    // block [0] is used for length + one more for PEC
-};
+#define I2C_BUS   0x0720
+#define I2C_SLAVE 0x0703
+#define I2C_READ  1
+#define I2C_WRITE 0
+#define I2C_BYTE  1
+#define I2C_DATA  2 
 
 struct i2c_ioctl_data
 {
-  char read_write;
-  uint8_t command;
-  int size;
-  union i2c_data *data;
+    char read_write;
+    uint8_t command;
+    int size;
+    uint8_t data;
 };
 
-static inline int i2c_access (int fd, char rw, uint8_t command, int size, union i2c_data *data)
+static inline int i2c_access (int fd, char rw, uint8_t command, int size, uint8_t data)
 {
-  struct i2c_ioctl_data args;
+    struct i2c_ioctl_data args;
 
-  args.read_write = rw;
-  args.command    = command;
-  args.size       = size;
-  args.data       = data;
-  return ioctl(fd, I2C_BUS, &args);
+    args.read_write = rw;
+    args.command    = command;
+    args.size       = size;
+    args.data       = data;     
+    return ioctl(fd, I2C_BUS, &args);
 }
 
-int write_i2c_register(int fd, int reg, int value)
+int write_i2c_register(int fd, int reg, uint8_t value)
 {
-  union i2c_data data ;
-
-  data.byte = value ;
-  return i2c_access(fd, I2C_BUS_WRITE, reg, I2C_BUS_BYTE_DATA, &data);
+  return i2c_access(fd, I2C_WRITE, reg, I2C_DATA, value);
 }
 
 int read_i2c(int fd)
 {
-  union i2c_data data;
-
-  if (i2c_access(fd, I2C_BUS_READ, 0, I2C_BUS_BYTE, &data))
-    return -1 ;
-  else
-    return data.byte & 0xFF;
+    uint8_t data = 0;
+    if (i2c_access(fd, I2C_READ, 0, I2C_BYTE, data))
+        return -1 ;
+    else
+        return data;
 }
 
 int setup_i2c(const char *device, int id)
 {
-  int fd = 0;
+    int fd = 0;
 
-  if ((fd = open(device, O_RDWR)) < 0)
-    return -1;
+    if ((fd = open(device, O_RDWR)) < 0)
+        return -1;
 
-  if (ioctl(fd, I2C_SLAVE, id) < 0)
-    return -1;
+    if (ioctl(fd, I2C_SLAVE, id) < 0)
+        return -1;
 
-  return fd ;
+    return fd ;
 }
 
 int main(void) {
@@ -100,7 +76,7 @@ int main(void) {
     int i;
     while(1) {
     
-        i2c_access(fd, I2C_BUS_WRITE, 0x00, I2C_BUS_BYTE, NULL) ;
+        i2c_access(fd, I2C_WRITE, 0x00, I2C_BYTE, 0) ;
         usleep(500);
     
         for (i=0; i<6; i++) {
